ee0d852fad472822cd18f3399b29539e71f6dc44
Sebastian Ilbay <atlantiscityplay@gmail.com>
2024-06-30 23:10:21 -0500

    CRUD géneros
    Se han implementado los métodos necesarios para generar los servicios CRUD de géneros

---
+++ backend/backend/Models/Genero.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Models
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set;}
+        public string Descripcion { get; set;} = string.Empty;
+    }
+}
